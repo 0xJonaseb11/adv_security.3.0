b2a39c5ec1fd908f393b6c02a293ce58c22349db
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-01 20:53:12 +0200

    update cross-function reentry

--- reentrancy/CrossFunctionReentrancy.sol
+++ reentrancy/CrossFunctionReentrancy.sol
@@ -6,6 +6,22 @@ pragma solidity ^0.8.19;
  */
 contract CrossFunctionReentrancy {
 
-    mapping(address => uint) public address
+    mapping(address => uint) private userBalances;
+
+    function transfer(address to, uint amount) public {
+        if (userBalances[msg.sender] >= amount) {
+            userBalances[to] += amount;
+            userBalances[msg.sender] -= amount;
+        }
+    }
+
+    function withdrawBalance() public {
+        uint amountToWithdraw = userBalances[msg.sender];
+        (bool success, ) = msg.sender.call{value: amountToWithdraw}("");
+           // At this point, the caller's code is executed, and can call transfer()
+           require(success, "Transfer failed");
+           userBalances[msg.sender] = 0;
+
+    }
 
 }
