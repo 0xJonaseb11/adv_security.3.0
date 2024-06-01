6c070c50f7adba02e114d659fb52ee39c88aef35
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-01 19:14:13 +0200

    update init cross-function reentrancy proned

---
+++ reentrancy/CrossFunctionReentrancy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @dev The below contract is prone to Cross-Function Reentrancy attack
+ */
+contract CrossFunctionReentrancy {
+
+    mapping(address => uint) public address
+
+}
